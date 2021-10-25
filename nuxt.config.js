a388aaba675dd7625a9e4834538b12e0b70b9413
Wands <wandsdeveloper@gmail.com>
2021-10-25 19:01:02 -0300

    deploy files

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/produto-teste/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
